d736ce06648de08a9521ca967cec43c7e62b17eb
dznleandro <leodzib100@gmail.com>
2023-06-14 09:20:19 -0600

    comentario

--- src/router.js
+++ src/router.js
@@ -20,7 +20,7 @@ const routes = [
         path: '/',
         name: 'home',
         component: () => import('./views/Home.vue'),
-        beforeEnter: requiredAuth
+        beforeEnter: requiredAuth //ruta protegida
     },
     {
         path: '/login',
